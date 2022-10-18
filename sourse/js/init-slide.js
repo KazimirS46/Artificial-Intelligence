a07fc89d6dc583d277f58da6fe9894a427f32f4a
Kazimir <KazimirS46@gmail.com>
2022-10-18 13:32:06 +0400

    Response Slider Done

--- sourse/js/init-slide.js
+++ sourse/js/init-slide.js
@@ -1,10 +1,34 @@
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-  slidesPerView: 5,
-  spaceBetween: 30,
+  breakpoints: {
+    425: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    769: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    901: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    1300: {
+      slidesPerView: 3,
+      spaceBetween: 25,
+    },
+    1920: {
+      slidesPerView: 5,
+      spaceBetween: 30,
+    },
+  },
 });
